4739ac374e68ec603914ad6c8c395ce02706c4a8
henriquesan14 <33522361+henriquesan14@users.noreply.github.com>
2018-08-17 13:55:19 -0300

    Update Livro.java

--- Livro.java
+++ Livro.java
@@ -4,13 +4,13 @@ public class Livro extends Produto {
 	private String autor;
 	
 	
-	
+	//metódo construtor passando como parâmetro os atributos da classe Livro e super classe Produto.
 	public Livro(int codigo,String nome,double preco,String autor) {
 		super(codigo,nome,preco);
 		this.autor = autor;
 	}
 
-	@Override
+	@Override // metódo toString sobrescrito pra mostrar todos atributos, inclusives o da superClasse Produto.
 	public String toString() {
 		return "Livro  [codigo: " + getCodigo() + "][nome: " + getNome() + "][preco: " + getPreco() + "][autor: " + autor + "]";
 	}
